29e993e489a9a6846ac551ea1e477f5b86ed5025
lmgraphix27-sudo <lmgraphix27@gmail.com>
2026-02-12 11:38:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+/* 
+  Navigation logic has been moved to inline script in index.html for simplicity 
+  with the single-file Tailwind approach, but you can add complex logic here if needed.
+*/
+console.log("LM Graphix website loaded.");
